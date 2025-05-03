7e0d0ce2a30db851b427d24beb076148dc6576b7
Urjagajera <urjagajera12@gmail.com>
2025-05-03 10:33:05 +0530

    Create noArg_noReturn.cpp
    Add function with no arguments and no return value

---
+++ functions/noArg_noReturn.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+     // A function that takes no arguments and returns nothing
+void hii() {
+    cout << "Hello! This function has no parameters and no return value." << endl;
+}
+
+int main() {
+    hii();    // Calling the function
+    return 0;
+}
