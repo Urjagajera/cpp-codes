759265265c7d7734e2bfcdf7076b2b4567301f2a
Urjagajera <urjagajera12@gmail.com>
2025-05-06 16:59:07 +0530

    inline_function.cpp

---
+++ functions/inline_function.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+// Inline function definition
+inline int square(int x) {
+    return x * x;
+}
+
+int main() {
+    int num = 5;
+    cout << "Square of " << num << " is: " << square(num) << endl;
+    return 0;
+}
